0ad3992f6fd7e1e2036165101d8b890c4d1954ac
msdpramod <kuppapramod@gmail.com>
2023-12-29 10:03:06 +0530

    spliwise

---
+++ src/main/java/org/example/sliptwise/SliptWiseApplication.java
@@ -0,0 +1,13 @@
+package org.example.sliptwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliptWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SliptWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sliptwise/SliptWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sliptwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliptWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
